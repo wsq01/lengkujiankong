40f7a78105bf2815329643e1ba2bfd37fa573b8b
wsq01 <592786982@qq.com>
2020-04-09 13:52:30 +0800

    first

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // allow async-await
+    'generator-star-spacing': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/no-parsing-error': [2, {
+      'x-invalid-end-tag': false
+    }],
+    "quotes": [1, "single"],
+    "semi": 0,
+    'no-undef': 'off',
+    'camelcase': 'off'
+  }
+}
